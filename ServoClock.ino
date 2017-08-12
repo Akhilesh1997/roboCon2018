761c496daa0f46aeee5019ed2bad6c81bad734ea
Akhilesh1997 <akhileshiyer.14.ce@iite.indusuni.ac.in>
2017-08-12 12:10:17 +0530

    Add files via upload
    I have made a clock-like program with the Servo..

---
+++ ServoClock.ino
@@ -0,0 +1,26 @@
+#include<Servo.h>
+Servo motor;
+void setup() {
+ Serial.begin(9600);
+ motor.attach(9);
+ motor.write(90);
+}
+
+void loop() {
+  int hand = 0;
+  for(; hand < 180; hand++){
+    tickTock(hand);
+    delay(10);
+  }
+
+  for(; hand > 0; hand--){
+    tickTock(hand);
+    delay(10);
+  }
+}
+
+void tickTock(int hand){
+  motor.write(hand);
+  if(hand%10 == 0)
+    delay(1000);
+  }
